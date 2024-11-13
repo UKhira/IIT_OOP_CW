9a6cbddc84c6405981df7308c4f00efadb5d73a2
Udith Kavishka <106836713+UKhira@users.noreply.github.com>
2024-11-14 00:36:56 +0530

    enhanced a bit

--- 1. CLI/Java CLI/src/main/java/CLI.java
+++ 1. CLI/Java CLI/src/main/java/CLI.java
@@ -4,18 +4,23 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Random;
 
 public class CLI{
 
     private static Scanner userInput = new Scanner(System.in);
     private static boolean inputPassed = false;
-    private static String[] optionArray = {"Welcome, Please select an option", "1. View Total Tickets", "2. Setup Ticket Release Rate", "3. Setup Customer Retrieval Rate", "4. Setup Max Ticket Capacity", "5. Exit and Start The System"};
+    private static String[] optionArray = {
+            "*  Welcome, Please select an option       *",
+            "*  1. View Total Tickets                  *",
+            "*  2. Setup Ticket Release Rate           *",
+            "*  3. Setup Customer Retrieval Rate       *",
+            "*  4. Setup Max Ticket Capacity           *",
+            "*  5. Exit and Start The System           *"};
+    private static int customerRetrieval,ticketRelease;
 
 
     // ** Initiate User Terminal Options ** //
     public static void main(String[] args) throws IOException {
-        TicketPool ticketPool = new TicketPool();
         while (!inputPassed) {
             try {
                 showOptions();
@@ -26,9 +31,9 @@ public class CLI{
                         System.out.println("There are " + TicketPool.getCurrentAmount() + " tickets currently available in system");
                         updateOptionMenu(1);
                         break;
-                    case 2: // Do something
+                    case 2: setTicketReleaseRate();
                         break;
-                    case 3: // Do something
+                    case 3: setCustomerRetrievalRate();
                         break;
                     case 4:
                         setMaxTickets();
@@ -38,7 +43,6 @@ public class CLI{
                         break;
                     default:
                         System.out.println("Invalid Input");
-                        inputPassed = false;
                         break;
                 }
             } catch (Exception exception) {
@@ -52,9 +56,17 @@ public class CLI{
     }
 
     private static void showOptions(){
+        for (int i = 0; i <= 10; i++){
+            System.out.print("*\t");
+        }
+        System.out.println();
         for (String option : optionArray) {
             System.out.println(option);
         }
+        for (int i = 0; i <= 10; i++){
+            System.out.print("*\t");
+        }
+        System.out.println();
     }
 
     private static void updateOptionMenu(int index){
@@ -73,31 +85,53 @@ public class CLI{
         }
     }
 
-    private static void startThreads() throws SQLException {
-        // Connect to Database
-        Database.connect();
+    private static void setTicketReleaseRate(){
+        System.out.println("Enter the Highest Limit Ticket Amount which a Vendor can add");
+        if(userInput.hasNextInt()) {
+            updateOptionMenu(2);
+            ticketRelease = userInput.nextInt();
+        }
+        else
+            System.out.println("Invalid input");
+    }
 
-        // Generate Random Ticket Amounts
-        Random randomNumber = new Random();
+    private static void setCustomerRetrievalRate(){
+        System.out.println("Enter the Highest Limit Ticket Amount which a Customer can buy");
+        if(userInput.hasNextInt()) {
+            updateOptionMenu(3);
+             customerRetrieval = userInput.nextInt();
+        }
+        else
+            System.out.println("Invalid input");
+    }
+
+    private static void startThreads() throws SQLException {
 
         // Check whether all the tasks is done in option menu
         int checker = 0;
-        /*for (String option : optionArray) {
+        for (String option : optionArray) {
             if(option.contains("Done"))
                 checker++;
-        }*/
-//        if(checker == 4){
+        }
+        if(checker == 4){
+            // Stop the Option Menu
             inputPassed = true;
+
+            // Connect to Database
+            Database.connect();
+
+            // Generate Random Ticket Amounts
+            Random randomNumber = new Random();
             TicketPool ticketPool = new TicketPool();
 
             // Customer and Vendor Threads
             for(int i = TicketPool.getCurrentAmount(); i < TicketPool.getMaxCapacity(); i++){
-                new Thread(new Vendor(ticketPool, randomNumber.nextInt(10))).start();
-                new Thread(new Customer(ticketPool, randomNumber.nextInt(10))).start();
+                new Thread(new Vendor(ticketPool, randomNumber.nextInt(1,ticketRelease))).start();
+                new Thread(new Customer(ticketPool, randomNumber.nextInt(1,customerRetrieval))).start();
             }
-        /*}
+        }
         else
-            System.out.println("Please setup all parameters before start the system");*/
+            System.out.println("Please setup all parameters before start the system");
     }
 }
 
